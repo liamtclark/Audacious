54a360c434cce8e94dc49b3eb8a1e085edf26974
Gareth Nunns <garethnunnd@gmail.com>
2013-12-23 16:16:01 +0000

    More standardisations
    Changed a load of the tags to improve SEO, etc.

--- index.php
+++ index.php
@@ -8,29 +8,24 @@
 	</head>
 	<!--Page-->
 	<body>
-		<div id="content-wrapper">
-			<span class="title">Audacious Productions</span>
-			<br/>
-			<span class="sub-title"><b>@ Comberton Village College</b></span>
-			<br/><br/>
+		<div id="wrapper">
+			<header>
+				<h1>Audacious Productions</h1>
+				<h2>@ Comberton Village College</h2>
 				<nav>
-				<a href="#" class="current">Home</a>
-				<a href="#">About</a>
-				<a href="#">Latest Show</a>
-				<a href="#">Previous Shows</a>
-				<a href="#">Gallery</a>
-				<a href="#">Tickets</a>
-				<a href="#">Contact</a>
+					<a href="#" class="current">Home</a>
+					<a href="#">About</a>
+					<a href="#">Latest Show</a>
+					<a href="#">Previous Shows</a>
+					<a href="#">Gallery</a>
+					<a href="#">Tickets</a>
+					<a href="#">Contact</a>
 				</nav>
+			</header>
+			<section>
+				<h2>Welcome.</h2>
+				<p>Welcome to the Audacious Productions website. Audacious Productions is a theatre group that runs at Comberton Village College.</p>
+			</section>
 		</div>
-			<br/><br/><br/><br/>
-			<div id="top-box" class="box">
-				<span class="heading">Welcome.</span>
-				<br/>
-				<b>Welcome to the Audacious Productions website. Audacious Productions is a theatre group that runs at Comberton Village College.</b>
-				
-			</div>
-		</div>
-		
 	</body>
 </html>

f43b541ca8575a9f8560193f57d429fbd161e830
liamtclark <lclark@live.co.uk>
2013-12-23 15:51:58 +0000

    Create index.php
    Start (Basically, the header is done)

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Audacious Productions | Home</title>
+		
+		<link rel="stylesheet" type="text/css" href="global.css"/>
+		<link href='http://fonts.googleapis.com/css?family=Mouse+Memoirs' rel='stylesheet' type='text/css'>
+	</head>
+	<!--Page-->
+	<body>
+		<div id="content-wrapper">
+			<span class="title">Audacious Productions</span>
+			<br/>
+			<span class="sub-title"><b>@ Comberton Village College</b></span>
+			<br/><br/>
+				<nav>
+				<a href="#" class="current">Home</a>
+				<a href="#">About</a>
+				<a href="#">Latest Show</a>
+				<a href="#">Previous Shows</a>
+				<a href="#">Gallery</a>
+				<a href="#">Tickets</a>
+				<a href="#">Contact</a>
+				</nav>
+		</div>
+			<br/><br/><br/><br/>
+			<div id="top-box" class="box">
+				<span class="heading">Welcome.</span>
+				<br/>
+				<b>Welcome to the Audacious Productions website. Audacious Productions is a theatre group that runs at Comberton Village College.</b>
+				
+			</div>
+		</div>
+		
+	</body>
+</html>
